fd8b862c77e1799e8a35634556717d32fae7b9a5
amina3033wayes <wayes.amina@gmail.com>
2018-05-28 14:21:30 +0600

    Pushed initial file

--- app/src/main/java/com/example/dell/myapplication_28/MainActivity.java
+++ app/src/main/java/com/example/dell/myapplication_28/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("I am in  : ","onClick");
+        Log.i("I am in  : ","onCreate");
     }
 
     @Override
