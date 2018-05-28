ccec68b1c63c148ebc9185e373631f83fca2551f
amina3033wayes <wayes.amina@gmail.com>
2018-05-28 14:10:36 +0600

    Real Activity Life Cycle

---
+++ app/src/main/java/com/example/dell/myapplication_28/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.dell.myapplication_28;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("I am in  : ","onClick");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("I am in  : ","onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("I am in  : ","onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("I am in  : ","onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("I am in  : ","onRestart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("I am in  : ","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("I am in  : ","onDestroy");
+    }
+}
